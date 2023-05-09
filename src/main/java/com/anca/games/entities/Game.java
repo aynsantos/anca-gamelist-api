35f8075abf28e34c1f0c961c257c80871f8b09be
aynsantos <allyson.santos@live.com>
2023-05-09 11:33:53 -0300

    Profile test configuration, h2database, entity game and ORM

---
+++ src/main/java/com/anca/games/entities/Game.java
@@ -0,0 +1,30 @@
+package com.anca.games.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Data
+@Entity
+@Table(name = "tb_games")
+public class Game {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    @Column(name = "game_year")
+    private Integer year;
+    private String genre;
+    private String platform;
+    private String imgUrl;
+    private String shortDescription;
+    private String longDescription;
+
+}
